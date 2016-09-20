2c3445966fc10087fdebf98d3f5469cacdab8e5a
Sam Chi <samuel.sj.chi@gmail.com>
2016-09-20 15:44:52 -0700

    [setup] - sets up App parent component, adds additional routes, and cleans up code

--- client/src/index.js
+++ client/src/index.js
@@ -1,24 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import reduxThunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { Router, browserHistory, Route, IndexRoute } from 'react-router';
-import reduxThunk from 'redux-thunk';
+import { Router, browserHistory } from 'react-router';
 
+// routes
 import routes from './routes';
-import Landing from './components/Landing';
+// reducers
+import reducers from './reducers';
 
-import reducers  from './reducers';
-
-
-const createStoreWithMiddleWare = applyMiddleware(reduxThunk)(createStore)
-const storey = createStoreWithMiddleWare(reducers)
-
+const createStoreWithMiddleWare = applyMiddleware(reduxThunk)(createStore);
+const store = createStoreWithMiddleWare(reducers);
 
 ReactDOM.render(
-  <Provider store={storey}>
+  <Provider store={store}>
     <Router history={browserHistory}> 
-    	{routes} 
+      {routes}
     </Router>
   </Provider>
-  , document.querySelector('.container'));
+  , document.querySelector('.container')
+);
