4ccd4a36f4f8177d85ad33dc794c94d851c46237
= <Jmitchell1991@gmail.com>
2016-09-20 13:41:37 -0700

    [feature] - creates postQuestionListInput action creator inside QuestionListDetail component

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,7 +1,26 @@
+import axios from 'axios';
+
 export function answerQuestion(question) {
 	console.log('we are inside answerQuestion')
 	return {
 		type: 'ANSWER_SELECTED',
 		payload: question
 	} 
+}
+
+export function postQuestionListInput(input) {
+	console.log('we are in postQuestionListInput')
+	return function(dispatch) {
+		axios.get('https://jsonplaceholder.typicode.com/posts')
+		.then(function(data) {
+			console.log('recevied data inside actions: ',data)
+			dispatch({
+				type: 'RECEIVED_DATA',
+				payload: data
+			})
+		})
+		.catch(function(err) {
+			console.log('error inside postQuestionListInput inside actions: ',err)
+		})
+	}
 }
--- client/src/containers/Question_List_Detail.jsx
+++ client/src/containers/Question_List_Detail.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { activeQuestion } from '../actions/index';
+import { activeQuestion, postQuestionListInput } from '../actions/index';
 import { bindActionCreators } from 'redux';
 
 class QuestionDetail extends Component {
 	constructor(props) {
 		super(props)
+
 	}
 	render() {
 		if (!this.props.question) {
@@ -15,6 +16,8 @@ class QuestionDetail extends Component {
 			<div>
 				<h3>Answered Question</h3>
 				<div>Selected Answer is: {this.props.question.answer}</div>
+				<button onClick={()=> this.props.postQuestionListInput(this.props.question.answer)}>Click to send</button>
+				<p>received data: {this.props.received}</p>
 			</div>
 			)
 	}
@@ -22,9 +25,14 @@ class QuestionDetail extends Component {
 
 function mapStateToProps(state) {
 	return {
-		question: state.activeQuestion
+		question: state.activeQuestion,
+		received: state.received
 	}
 }
 
-export default connect(mapStateToProps)(QuestionDetail)
+function mapDispatchToProps(dispatch) {
+	return bindActionCreators({ postQuestionListInput: postQuestionListInput}, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(QuestionDetail)
 
