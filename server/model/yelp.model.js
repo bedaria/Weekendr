50de8de3b1ce8e5ec9e84d80d0a23b14d7c66882
= <Jmitchell1991@gmail.com>
2016-09-19 20:57:37 -0700

    [setup] - creates yelp {Router, Model, Controller}

---
+++ server/model/yelp.model.js
@@ -0,0 +1,10 @@
+var _ = require('underscore');
+
+var yelpModel = module.exports
+
+yelpModel.getActivities = function() {
+	//do work for the function
+	return new Promise(resolve, reject) {
+		//make function call and then resolve it
+	}
+}
