ec259115bed810c8c4709412defbc029c9ff227a
bedaria <dburinsk@gmail.com>
2016-09-21 13:00:00 -0700

    [pull] - uncommented yelp routes

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const request = require('request');
 const bodyParse = require('body-parser');
 
-// const yelpRouter = require('./router/yelpRouter');
+const yelpRouter = require('./router/yelpRouter');
 
 require('./database.js');
 require('./routes.js')(app, express);
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(bodyParse.json({limit: '50mb'}));
 app.use(express.static('./client'));
 
-// app.use('/api/yelp', yelpRouter)
+app.use('/api/yelp', yelpRouter)
 
 app.get('/', (request, response) => response.sendFile('index.html'));
 
