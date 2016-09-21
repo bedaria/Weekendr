14c69ee56103093940a73aebeb96b7bb27535d4b
bedaria <dburinsk@gmail.com>
2016-09-21 12:03:15 -0700

    [pull] - connects database to server

--- server/server.js
+++ server/server.js
@@ -5,13 +5,13 @@ var cors = require('cors');
 var request = require('request');
 var bodyParse = require('body-parser');
 var yelpRouter = require('./router/yelpRouter')
-
+require('./database.js')
 
 app.use(cors());
 app.use(bodyParse.json({limit: '50mb'}));
 app.use(express.static('./client'));
 
-app.use('/api/yelp', yelpRouter)
+// app.use('/api/yelp', yelpRouter)
 
 app.get('/', (request, response) => {
   response.sendFile(path.resolve(__dirname,'../index.html'));
