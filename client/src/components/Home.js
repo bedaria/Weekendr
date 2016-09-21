bd7cf8fa8f27d54ff384458bcb4b2d0a7ef94cd0
= <Jmitchell1991@gmail.com>
2016-09-21 13:06:22 -0700

    [fix] - removes QuizList from home

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,13 +1,10 @@
 import React, { Component } from 'react';
 
-import QuizList from '../containers/Quiz_List';
-
 export default class Home extends Component {
   render() {
     return (
       <div>
         <h1>Welcome to Weekendr</h1>
-        <QuizList />
       </div>
     );
   }
