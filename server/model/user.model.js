b6cceda0f82c8da4211697847d12441f14fa6fc6
bedaria <dburinsk@gmail.com>
2016-09-21 12:46:31 -0700

    [pull] - creates user model

---
+++ server/model/user.model.js
@@ -0,0 +1,32 @@
+const Sequelize = require('sequelize');
+const sequelize = require('../database');
+
+const User = sequelize.define('user', {
+  userName: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  firstName: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  lastName: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  password: {
+    type: Sequelize.STRING,
+    allowNull: false
+  }
+});
+
+User
+  .sync()
+  .then(() => console.log('<Weekendr> User model sync() successful'))
+  .catch(error => console.log('<Weekendr> Couldn\'t sync() User model'));
+
+module.exports = User;
