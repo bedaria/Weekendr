8e1c859d13017f8dc0ab38fe1e8252af0060cb70
bedaria <dburinsk@gmail.com>
2016-09-21 19:58:56 -0700

    [pull] - adds signin container

---
+++ client/src/containers/Signin.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import axios from 'axios'
+
+class Signin extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      username: null,
+      password: null
+    }
+
+    this.updateState = this.updateState.bind(this)
+    this.signin = this.signin.bind(this)
+  }
+
+  updateState(event) {
+    this.setState({[event.target.id]: event.target.value})
+  }
+
+  signin(event) {
+    event.preventDefault()
+    axios.post('/api/signin', {
+      username: this.state.username,
+      password: this.state.password
+    })
+    .then(resp => {
+      if(resp.data === "ERROR") console.log("Error logging in")
+      else localStorage.setItem('token', resp.data.token)
+    })
+    .catch(err => console.log("POST /signin failed: ", err))
+  }
+
+  render() {
+    return (
+      <div>
+        <input type="text" id="username" onChange={this.updateState} placeholder="Input username"/>
+        <input type="password" id="password" onChange={this.updateState} placeholder="Input password"/>
+        <input type="submit" onClick={this.signin}/>
+      </div>
+    )
+  }
+}
+
+export default Signin;
