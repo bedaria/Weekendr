1084e32c135464110c308c66fc94820d0242b2ca
= <Jmitchell1991@gmail.com>
2016-09-24 12:27:01 -0700

    [refactor] - moves QuizList actions to actions/QuizList

--- client/src/actions/QuizList.js
+++ client/src/actions/QuizList.js
@@ -1,6 +1,8 @@
+import axios from 'axios';
+
 import {
 	ANSWER_SELECTED
-} from './actionTypes'
+} from './actionTypes';
 
 export function answerQuestion(answer, props, answersArr) {
 	console.log('we are inside QuizList action inside answerQuestion')
@@ -14,4 +16,21 @@ export function answerQuestion(answer, props, answersArr) {
 		type: ANSWER_SELECTED,
 		payload: {answers: props.quizAnswers, questionIndex: props.questionIndex+1}
 		}
+}
+
+
+export function postQuestionListInput(input) {
+	return dispatch => {
+		axios.get('https://jsonplaceholder.typicode.com/posts')
+		.then((data) => {
+			console.log('recevied data inside actions: ',data)
+			dispatch({
+				type: 'RECEIVED_DATA',
+				payload: data
+			})
+		})
+		.catch(function(err) {
+			console.log('error inside postQuestionListInput inside actions: ',err)
+		})
+	}
 }
--- client/src/containers/QuizList.jsx
+++ client/src/containers/QuizList.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { answerQuestion, postQuestionListInput } from '../actions/index';
+import { answerQuestion, postQuestionListInput } from '../actions/QuizList';
 import { bindActionCreators } from 'redux';
 import  QuizListItem  from '../components/QuizListItem'
 class QuizList extends Component {
