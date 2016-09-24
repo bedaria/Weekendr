8ae82057671b596239d5cf2a1f1b0d6261aa763a
= <Jmitchell1991@gmail.com>
2016-09-24 14:04:35 -0700

    [fix] - fixes div's on userInput component

--- client/src/reducers/coordinatesReducer.js
+++ client/src/reducers/coordinatesReducer.js
@@ -1,14 +1,10 @@
-const initialValue = {
-	latitude: 0,
-	longitude: 0
-}
 
-const coordinatesReducer = (state = initialValue, action) => {
+const coordinatesReducer = (state = [], action) => {
 	switch(action.type) {
 		case 'LAT_LONG':
 			console.log(action.payload, "inside of cooodinatesReducer");
 			var item = { ...state, 
-				coordinates: action.payload};
+				coordinates: action.payload && action.payload};
 			console.log("what is this item?", item);
 			return item;
 		default: return state
