5e284f6e8b268846c36496694921b07e33967c0d
= <Jmitchell1991@gmail.com>
2016-09-23 19:59:50 -0700

    [feature] - creates SidebarPreferenceItem and adds it to Sidebar's renderItem function

--- client/src/containers/SideBar.jsx
+++ client/src/containers/SideBar.jsx
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { postQuestionListInput } from '../actions/index';
 import { bindActionCreators } from 'redux';
-import SidebarPreferences from '../components/SidebarPreferences'
+import SidebarPreferenceItem from '../components/SidebarPreferenceItem'
+
 
 class Sidebar extends Component {
 	constructor(props) {
@@ -13,18 +14,45 @@ class Sidebar extends Component {
 	renderSidebarDetail() {
 		return this.props.quizAnswers.map((q,index) => {
 			return (
-				<div className="SidebarDetail" key={index}>
-				<h1>Title: {q.title}</h1>
-				<h1>{q.option.option && q.option.option}</h1>
-					<img src={q.option.imageUrl}/>
-				</div>
+				<SidebarPreferenceItem 
+					key={index}
+					title={q.title} 
+					src={q.option.imageUrl}
+					answer={q.option.option} 
+				/>
 				)
 		})
 	}
 
+	// return (
+	// 			<QuizListItem 
+	// 				key={q.id}
+	// 				option={q.option} 
+	// 				src={q.imageUrl} 
+	// 				onAnswerClicked = {this.onAnswerClicked}
+	// 			/>
+	// 		)	
+
+
+
+
+
+
+		// return (
+		// 		<div className="SidebarDetail" key={index}>
+		// 		<h1>Title: {q.title}</h1>
+		// 		<h1>{q.option.option && q.option.option}</h1>
+		// 			<img src={q.option.imageUrl}/>
+		// 		</div>
+		// 		)
+
+
+
+
+
 	render() {
 		return (
-			<div className="Sidebar">
+			<div className="SidebarPreferences">
 			{this.renderSidebarDetail()}
 			</div>
 			)
