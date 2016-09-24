2516172119d72d7787e8c7a5b3752aac2a12e986
= <Jmitchell1991@gmail.com>
2016-09-24 14:09:03 -0700

    [fix] - changes string to var in QuizList actionType

--- client/src/reducers/reducer_QuizList.js
+++ client/src/reducers/reducer_QuizList.js
@@ -3,7 +3,7 @@ import { ANSWER_SELECTED } from '../actions/actionTypes'
 
 export default function(state = QuizListData, action) {
 	switch(action.type) {
-		case 'ANSWER_SELECTED':
+		case ANSWER_SELECTED:
 		return {...state, quizAnswers: action.payload.answers, questionIndex: action.payload.questionIndex};
 		default: return state
 	}
