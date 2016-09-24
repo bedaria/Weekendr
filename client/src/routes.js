694ebb92ed09d6066377fd2443a798aaff487bef
Sam Chi <samuel.sj.chi@gmail.com>
2016-09-24 14:28:24 -0700

    [pull] - adds Preferences component which includes Sidebar and QuizList

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
-
 import App from './components/App';
 import Home from './components/Home';
+import Preferences from './components/Preferences';
+import UserProfile from './containers/UserProfile';
 import Signup from './containers/Signup';
 import Login from './containers/Signin';
-import QuizList from './containers/QuizList'
 // import TripResults from './components/TripResults';
-import UserProfile from './containers/UserProfile';
 
 //routes to be added:
 // <Route path="/trip-results" component={TripResults} />
@@ -16,7 +15,7 @@ import UserProfile from './containers/UserProfile';
 const routes = (
   <Route path="/" component={App}>
     <IndexRoute component={Home} />
-    <Route path="/quiz" component={QuizList} />
+    <Route path="/preferences" component={Preferences} />
     <Route path="/profile" component={UserProfile} />
     <Route path="/signup" component={Signup} />
     <Route path="/login" component={Login} />
