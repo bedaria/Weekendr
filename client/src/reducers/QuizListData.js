235e3a464af441239905b698e18f3dd4f60dae7f
Sam Chi <samuel.sj.chi@gmail.com>
2016-09-24 12:07:28 -0700

    [style] - updates Lodging preference options

--- client/src/reducers/QuizListData.js
+++ client/src/reducers/QuizListData.js
@@ -1,42 +1,41 @@
 export default {
-		quizList: [
-		{
-			title: 'Lodging',
-			options: [
-			{
-				id: 1,
-				option: 'Hotel',
-				imageUrl: '../assets/images/Hotel.jpg'
-			},
-			{
-				id: 2,
-				option: 'Hostel',
-				imageUrl: '../assets/images/Hostel.jpg'
-			},
-			{
-				id: 3,
-				option: 'AirBnb',
-				imageUrl: '../assets/images/airbnb.jpg'
-
-			}
-			]
-		},
-		{
-			title: 'Solitude or Big Crowds',
-			options: [
-			{
-				id: 3,
-				option: 'Solitude',
-				imageUrl: '../assets/images/Solitude.jpg'
-			},
-			{
-				id: 4,
-				option: 'Big Crowds',
-				imageUrl: '../assets/images/Rave.jpg'
-			},
-			]
-		}
-		],
-		quizAnswers: [],
-		questionIndex: 0
-	}
+  quizList: [
+    {
+      title: 'Lodging',
+      options: [
+        {
+          id: 1,
+          option: 'Hotel',
+          imageUrl: '../assets/images/hotel.png'
+        },
+        {
+          id: 2,
+          option: 'Hostel',
+          imageUrl: '../assets/images/hostel.png'
+        },
+        {
+          id: 3,
+          option: 'Airbnb',
+          imageUrl: '../assets/images/airbnb.png'
+        }
+      ]
+    },
+    {
+      title: 'Solitude or Big Crowds',
+      options: [
+        {
+          id: 3,
+          option: 'Solitude',
+          imageUrl: '../assets/images/Solitude.jpg'
+        },
+        {
+          id: 4,
+          option: 'Big Crowds',
+          imageUrl: '../assets/images/Rave.jpg'
+        },
+      ]
+    }
+  ],
+  quizAnswers: [],
+  questionIndex: 0
+};
