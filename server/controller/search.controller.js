dbd042e9867dbe43e17b82bf1f0047d924d85f82
= <Jmitchell1991@gmail.com>
2016-09-27 10:55:44 -0700

    [refactor] - removes console.log in search.controller

--- server/controller/search.controller.js
+++ server/controller/search.controller.js
@@ -3,7 +3,7 @@ const searchModel = require('../model/search.model');
 console.log('we are inside searchController');
 
 function getCity(req, res) {
-  console.log('*****req.body inside searchController inside getCity is: ', req.body);
+  // console.log('*****req.body inside searchController inside getCity is: ', req.body);
   searchModel.getCity(req.body)
     .then((cityList) => {
       // console.log('result inside search.getCity inside searchController is : ', cityList);
