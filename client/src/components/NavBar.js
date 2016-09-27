6651a606782a4e6d8083636f24680b6a6a8559ba
Sam Chi <samuel.sj.chi@gmail.com>
2016-09-26 21:13:59 -0700

    [feature] - updates NavBar and fixes React props bug in old NavBar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,27 +1,42 @@
-import React from 'react';
-import { Link } from 'react-router';
-import { Navbar, Nav, Button } from 'react-bootstrap';
+import React, { Component } from 'react';
+import { IndexLink } from 'react-router';
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 
-
-//routes to be added:
-// <Route path="/signup" component={Signup} />
-// <Route path="/login" component={Login} />
-// <Route path="/trip-results" component={TripResults} />
-
-//Links to be added
-    // <Link to="/about"><Button bsStyle="default" className="nav-button"> About Us </Button></Link>
-    // <Link to="/how-it-works"><Button bsStyle="default" className="nav-button"> How It Works </Button></Link>
-
-
-const NavBar = (props) => (
-  <Nav bsStyle="pills" className="button-bar">
-    <Link to="/"><Button bsStyle="default" className="nav-button"> Home </Button></Link>
-    <Link to="/"><img className="logo-center" /></Link>
-    <Link to="/preferences"><Button bsStyle="default" className="nav-button float-right pad-right"> Preferences </Button></Link>
-    <Link to="/profile"><Button bsStyle="default" className="nav-button float-right pad-right"> Profile </Button></Link>
-    <Link to="/signup"><Button bsStyle="default" className="nav-button float-right pad-right"> SignUp </Button></Link>
-    <Link to="/login"><Button bsStyle="default" className="nav-button float-right pad-right"> Login </Button></Link>
-  </Nav>
-);
-
-export default NavBar;
+export default class NavBar extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar className="navbar">
+          <Navbar.Header>
+            <Navbar.Brand>
+              <IndexLink to="/">Weekendr</IndexLink>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav>
+              <LinkContainer to={{ pathname: '#' }}>
+                <NavItem>How It Works</NavItem>
+              </LinkContainer>
+              <LinkContainer to={{ pathname: '#' }}>
+                <NavItem>Team</NavItem>
+              </LinkContainer>
+            </Nav>
+            <Nav pullRight>
+              <LinkContainer to={{ pathname: '/profile' }}>
+                <NavItem>Profile</NavItem>
+              </LinkContainer>
+              <LinkContainer to={{ pathname: '/signup' }}>
+                <NavItem>Sign Up</NavItem>
+              </LinkContainer>
+              <LinkContainer to={{ pathname: '/login' }}>
+                <NavItem>Login</NavItem>
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+}
