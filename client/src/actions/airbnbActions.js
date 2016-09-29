bc775b5e3b169a2dca8be84e6bb35c900cfcae7d
Sam Chi <samuel.sj.chi@gmail.com>
2016-09-29 13:02:45 -0700

    [pull] - temporarily fetching Airbnb listings on client-side, need to fix 404 bug on server-side

--- client/src/actions/airbnbActions.js
+++ client/src/actions/airbnbActions.js
@@ -2,15 +2,23 @@ import axios from 'axios';
 import { AIRBNB_LISTINGS } from './actionTypes';
 
 export function fetchAirbnbListings(props) {
-  const options = {
-    address: props.coordinates.address,
-    lat: props.coordinates.latitude,
-    lng: props.coordinates.longitude
-  };
+  const clientId = '3092nxybyb0otqw18e8nh5nty';
+  const address = props.coordinates.address;
+  const lat = props.coordinates.latitude;
+  const lng = props.coordinates.longitude;
+  const endpoint = `https://api.airbnb.com/v2/search_results?client_id=${clientId}&location=${address}&user_lat=${lat}&user_lng=${lng}`;
+
+  // const options = {
+  //   address: props.coordinates.address,
+  //   lat: props.coordinates.latitude,
+  //   lng: props.coordinates.longitude
+  // };
+
   return (dispatch) => {
-    axios.get('/api/airbnb/getListings', options)
+    axios.get(endpoint)
+    // axios.get('/api/airbnb/getListings', options)
       .then((data) => {
-        console.log('Data from axios get call inside airbnbActions:', data);
+        console.log('Data from axios.get call inside fetchAirbnbListings in airbnbActions:', data);
         dispatch({
           type: AIRBNB_LISTINGS,
           payload: data
