e676b10635f94a0a082be6ba1ba8d85f7a18c3ca
Sam Chi <samuel.sj.chi@gmail.com>
2016-09-29 12:35:25 -0700

    [pull] - adds fetchAirbnbListings action creator to Quiz, called when user selects Airbnb as lodging option

--- client/src/reducers/reducer_airbnb.js
+++ client/src/reducers/reducer_airbnb.js
@@ -6,6 +6,7 @@ export default function(state = null, action) {
       return {
         ...state,
         listings: action.payload.data.search_results
+      };
     default:
       return state;
   }
