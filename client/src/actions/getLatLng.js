df47c0aebd98d287d1eebf1555a2d2a132368545
Sam Chi <samuel.sj.chi@gmail.com>
2016-09-29 12:02:22 -0700

    [pull] - adds geocode address to Redux state for use in Airbnb listings API call

--- client/src/actions/getLatLng.js
+++ client/src/actions/getLatLng.js
@@ -1,27 +1,43 @@
 // var request = require("request");
+import axios from 'axios';
 import { LAT_LONG } from './actionTypes';
 
 export function getLatLng() {
   return (dispatch) => {
     var lat = 0;
-	  var long = 0;
-	  var airport = '';
+    var long = 0;
+    var airport = '';
+
     function showLocation(positionA){
-      console.log('Latitude: ' + positionA.coords.latitude + ' Longitude: ', +positionA.coords.longitude);
+      console.log('Latitude: ' + positionA.coords.latitude + ' Longitude: ' + positionA.coords.longitude);
+
       lat = positionA.coords.latitude;
       long = positionA.coords.longitude;
-		    // findAirport(lat, long);
-      var data = {
-		    latitude: lat,
-        longitude: long,
-        airport: airport,
-		  };
-      dispatch({
-        type: 'LAT_LONG',
-        payload: data,
-      });
+        // findAirport(lat, long);
+
+      const GEOCODE_API_KEY = 'AIzaSyCBUyBZfRmKN4pPHQXBL8YVoK3CLrYUbnA';
+      const geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${long}&key=${GEOCODE_API_KEY}`;
+
+      axios.get(geocodeUrl)
+        .then((geocodeObj) => {
+          const address = geocodeObj.data.results[0].formatted_address;
+          var data = {
+            latitude: lat,
+            longitude: long,
+            address: address,
+            airport: airport,
+          };
+
+          dispatch({
+            type: LAT_LONG,
+            payload: data,
+          });
+        })
+        .catch((error) => {
+          console.error('Error inside axios get request for geocode address:', error);
+        });
     }
-	  //  	function findAirport(lat, long){
+    //  	function findAirport(lat, long){
 	  //  		var url = 'https://airport.api.aero/airport/nearest/' + lat + '/' + long;
 			// var options = {
 			//   method: 'GET',
