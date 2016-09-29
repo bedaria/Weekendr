3c326cee2e263971e1684139f00ac14848eb2d6a
Sam Chi <samuel.sj.chi@gmail.com>
2016-09-29 13:07:02 -0700

    [feature] - adds AirbnbListItem thumbnail component to render listings

--- client/src/containers/AirbnbList.jsx
+++ client/src/containers/AirbnbList.jsx
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import AirbnbListItem from '../components/AirbnbListItem';
 
 class AirbnbList extends Component {
   renderListings() {
     return this.props.airbnb.listings.map((listing) => {
       return (
-        <div>{listing.listing.name}</div>
+        <AirbnbListItem
+          key={listing.listing.id}
+          thumbnail={listing.listing.picture_url}
+          name={listing.listing.name}
+          price={listing.pricing_quote.localized_nightly_price}
+          city={listing.listing.city}
+          capacity={listing.listing.person_capacity}
+          beds={listing.listing.bedrooms}
+          baths={listing.listing.bathrooms}
+        />
       );
     });
   }
