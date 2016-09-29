1e16a0f0e9e385c16c13315481f3238e242f1c05
Sam Chi <samuel.sj.chi@gmail.com>
2016-09-29 13:02:45 -0700

    [pull] - temporarily fetching Airbnb listings on client-side, need to fix 404 bug on server-side

--- client/src/actions/airbnbActions.js
+++ client/src/actions/airbnbActions.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import { AIRBNB_LISTINGS } from './actionTypes';
 
 export function fetchAirbnbListings(props) {
+<<<<<<< e8607d8d1d2c0a995832fe7c8b60539088cc1330
   const options = {
     address: props.coordinates.address,
     lat: props.coordinates.latitude,
@@ -12,6 +13,25 @@ export function fetchAirbnbListings(props) {
     axios.post('/api/airbnb/getListings', options)
       .then((data) => {
         console.log('Data from axios call inside fetchAirbnbListings in airbnbActions:', data);
+=======
+  const clientId = '3092nxybyb0otqw18e8nh5nty';
+  const address = props.coordinates.address;
+  const lat = props.coordinates.latitude;
+  const lng = props.coordinates.longitude;
+  const endpoint = `https://api.airbnb.com/v2/search_results?client_id=${clientId}&location=${address}&user_lat=${lat}&user_lng=${lng}`;
+
+  // const options = {
+  //   address: props.coordinates.address,
+  //   lat: props.coordinates.latitude,
+  //   lng: props.coordinates.longitude
+  // };
+
+  return (dispatch) => {
+    axios.get(endpoint)
+    // axios.get('/api/airbnb/getListings', options)
+      .then((data) => {
+        console.log('Data from axios.get call inside fetchAirbnbListings in airbnbActions:', data);
+>>>>>>> [pull] - temporarily fetching Airbnb listings on client-side, need to fix 404 bug on server-side
         dispatch({
           type: AIRBNB_LISTINGS,
           payload: data
