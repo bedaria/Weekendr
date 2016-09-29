7c470eae076dbe1d5e85218050ea4fc48d1b86b0
Sam Chi <samuel.sj.chi@gmail.com>
2016-09-29 11:42:55 -0700

    [fix] - fixes travel date not rendering in sidebar

--- client/src/components/SidebarUserInput.jsx
+++ client/src/components/SidebarUserInput.jsx
@@ -7,7 +7,7 @@ export default class SidebarUserInput extends Component {
         <h6><strong>Your Trip Details:</strong></h6>
         <p>Budget: {this.props.budget}</p>
         <p>Number of Travelers: {this.props.numTravelers}</p>
-        <p>Travel Date: {this.props.datePicker}</p>
+        <p>Travel Date: {this.props.date}</p>
       </div>
     );
   }
