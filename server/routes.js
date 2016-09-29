214fd85cfed98ddef25c1e096532a37ef7ed00bc
Sam Chi <samuel.sj.chi@gmail.com>
2016-09-29 11:22:36 -0700

    [pull] - adds Airbnb listings route in routes.js

--- server/routes.js
+++ server/routes.js
@@ -1,16 +1,23 @@
-const userController = require('./controller/user.controller.js');
-// const yelpController = require('./controller/yelp.controller.js');
-const receiveController = require('./controller/receive.controller.js');
-
-const searchController = require('./controller/search.controller.js');
-const googleFlightsController = require('./controller/googleFlights.controller.js');
+const userController = require('./controller/user.controller');
+const receiveController = require('./controller/receive.controller');
+const searchController = require('./controller/search.controller');
+const airbnbController = require('./controller/airbnb.controller');
+const googleFlightsController = require('./controller/googleFlights.controller');
 
 module.exports = (app, express) => {
+  // user
   app.post('/api/createUser', userController.user.createUser);
   app.post('/api/signin', userController.user.authenticateUser);
-  app.post('/api/googleFlights', googleFlightsController.googleFlights.getFlights);
 
-  app.post('/api/search/getCity', searchController.search.getCity);
+  // receive
   app.post('/api/receive/receiveCity', receiveController.receive.receiveCity);
 
-}
+  // search
+  app.post('/api/search/getCity', searchController.search.getCity);
+
+  // google flights
+  app.post('/api/googleFlights', googleFlightsController.googleFlights.getFlights);
+
+  // airbnb
+  app.post('api/airbnb/getListings', airbnbController.airbnb.getListings);
+};
