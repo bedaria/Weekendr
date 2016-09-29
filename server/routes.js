6534978dd31168b4a3ca677970198bfa7c5c707c
Sam Chi <samuel.sj.chi@gmail.com>
2016-09-29 11:22:36 -0700

    [pull] - adds Airbnb listings route in routes.js

--- server/routes.js
+++ server/routes.js
@@ -1,13 +1,19 @@
-const userController = require('./controller/user.controller.js');
-// const yelpController = require('./controller/yelp.controller.js');
-const receiveController = require('./controller/receive.controller.js');
-const searchController = require('./controller/search.controller.js');
+const userController = require('./controller/user.controller');
+const receiveController = require('./controller/receive.controller');
+const searchController = require('./controller/search.controller');
+const airbnbController = require('./controller/airbnb.controller');
 
 module.exports = (app, express) => {
+  // user routes
   app.post('/api/createUser', userController.user.createUser);
   app.post('/api/signin', userController.user.authenticateUser);
 
-
-  app.post('/api/search/getCity', searchController.search.getCity);
+  // receive routes
   app.post('/api/receive/receiveCity', receiveController.receive.receiveCity);
-}
+
+  // search routes
+  app.post('/api/search/getCity', searchController.search.getCity);
+
+  // airbnb routes
+  app.get('api/airbnb/getListings', airbnbController.airbnb.getListings);
+};
