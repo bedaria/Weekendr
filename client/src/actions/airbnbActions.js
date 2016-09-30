d854d83c61da15cc3945b2b6d523fcc9e0732ef3
Sam Chi <samuel.sj.chi@gmail.com>
2016-09-29 17:08:10 -0700

    [refactor] - makes axios call to back-end API endpoint in Airbnb action creator

--- client/src/actions/airbnbActions.js
+++ client/src/actions/airbnbActions.js
@@ -2,23 +2,16 @@ import axios from 'axios';
 import { AIRBNB_LISTINGS } from './actionTypes';
 
 export function fetchAirbnbListings(props) {
-  const clientId = '3092nxybyb0otqw18e8nh5nty';
-  const address = props.coordinates.address;
-  const lat = props.coordinates.latitude;
-  const lng = props.coordinates.longitude;
-  const endpoint = `https://api.airbnb.com/v2/search_results?client_id=${clientId}&location=${address}&user_lat=${lat}&user_lng=${lng}`;
-
-  // const options = {
-  //   address: props.coordinates.address,
-  //   lat: props.coordinates.latitude,
-  //   lng: props.coordinates.longitude
-  // };
+  const options = {
+    address: props.coordinates.address,
+    lat: props.coordinates.latitude,
+    lng: props.coordinates.longitude
+  };
 
   return (dispatch) => {
-    axios.get(endpoint)
-    // axios.get('/api/airbnb/getListings', options)
+    axios.post('/api/airbnb/getListings', options)
       .then((data) => {
-        console.log('Data from axios.get call inside fetchAirbnbListings in airbnbActions:', data);
+        console.log('Data from axios call inside fetchAirbnbListings in airbnbActions:', data);
         dispatch({
           type: AIRBNB_LISTINGS,
           payload: data
