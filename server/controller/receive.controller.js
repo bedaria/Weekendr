63006a0bb634299307fee92d0a7574fb9cedd188
Sam Chi <samuel.sj.chi@gmail.com>
2016-10-01 15:24:16 -0700

    [pull] - adds Airbnb model to promise array in receive controller

--- server/controller/receive.controller.js
+++ server/controller/receive.controller.js
@@ -5,33 +5,33 @@ const googleFlights = require('../model/googleFlights.model');
 const airbnbModel = require('../model/airbnb.model');
 const expediaHotelsModel = require('../model/expediaHotels.model');
 
-
-
-/****** REQ.BODY  ********
- req.body inside receiveCity { name: 'Tijuana',
-  id: 3981609,
-  lat: 32.502698,
-  lng: -117.003714,
+/* ******** REQ.BODY ********
+req.body inside receiveCity:
+{ name: 'Los Angeles',
+  id: 5368361,
+  lat: 34.0522342,
+  lng: -118.2436849,
   answers:
    [ { title: 'Transportation', option: [Object] },
      { title: 'Lodging', option: [Object] },
      { title: 'Outdoor Activities', option: [Object] },
      { title: 'Indoor Activities', option: [Object] } ],
-  budget: '500',
+  budget: '400',
   numTravelers: '3',
-  date: '2016-12-31',
+  date: '2016-10-01',
   origin:
-   { latitude: 34.019224699999995,
-     longitude: -118.49427560000001,
+   { latitude: 34.019292799999995,
+     longitude: -118.49436989999998,
+     address: '604 Arizona Ave, Santa Monica, CA 90401, USA',
      airport: '' } }
-  * ******************/
+****************** */
 
-// NEW RECEIVE CITY PROMISIED
+// NEW RECEIVE CITY PROMISIFIED
 function receiveCity(req, res) {
-  // console.log('inside receiveCity req.body is : ', req.body);
+  console.log('inside receiveCity req.body is : ', req.body);
   const promiseArray = [];
 
-  //follow the same pattern below for insertion of Car, Airplane, and Train Models
+  // TRANSPORTATION
   const first = _.first(req.body.answers);
   if (first.title === 'Transportation') {
     if (first.option.option === 'Car') {
@@ -44,25 +44,16 @@ function receiveCity(req, res) {
     }
   }
 
+  // LODGING
   const second = req.body.answers[1];
-  if (second.title === 'Lodging') {
-    if (second.option.option === 'Hostel') {
-      console.log('inside receivedCity we received Hostel');
-      // push invoked Hostel model into promiseArray
-      // promiseArray.push()
-    } else if (second.option.option === 'Airbnb') {
-      console.log('inside receivedCity we received Airbnb');
-      // SAM
-      // push invoked Airbnb model into promiseArray
-      // promiseArray.push()
-    } else if (second.option.option === 'Hotel') {
-      console.log('inside receivedCity we received Hotel');
-      promiseArray.push(expediaHotelsModel.findHotels(req.body))
-      // Daria
-      // push invoked Hotel model into promiseArray
-      // promiseArray.push()
-    }
+  if (second.option.option === 'Airbnb') {
+    console.log('inside receivedCity we received Airbnb');
+    promiseArray.push(airbnbModel.getListings(req.body));
+  } else if (second.option.option === 'Hotel') {
+    console.log('inside receivedCity we received Hotel');
+    promiseArray.push(expediaHotelsModel.findHotels(req.body));
   }
+
   const answers = req.body.answers.slice(2);
   const fourSquarePromises = answers.map((answer) => {
     return fourSquareModel.explore(req.body, answer.option.id);
@@ -70,19 +61,18 @@ function receiveCity(req, res) {
   const newPromiseArray = promiseArray.concat(fourSquarePromises);
 
   Promise.all(newPromiseArray)
-  .then((data) => {
-    // console.log('dataArr after promiseArray: ', dataArr);
-    // insert data handlers
+    .then((data) => {
+      console.log('dataArr after promiseArray: ', data);
+      // insert data handlers
 
-    // manipulate data and change it to bundle
-    // res.status(200).send(bundle) <--
-    res.status(200).send(data);
-
-  })
-  .catch((err) => {
-    console.log('error inside receiveCity: ', err);
-    res.status(500).end(err);
-  });
+      // manipulate data and change it to bundle
+      // res.status(200).send(bundle) <--
+      res.status(200).send(data);
+    })
+    .catch((err) => {
+      console.log('error inside receiveCity: ', err);
+      res.status(500).end(err);
+    });
 }
 
 
