d9289e7a2f7e60cf92f29cf4fa5abb6ab8d87a26
bedaria <dburinsk@gmail.com>
2016-10-04 23:06:18 -0700

    [fix] - puts createBundles in own file, fixes bug

--- server/controller/createBundles.js
+++ server/controller/createBundles.js
@@ -1,11 +1,8 @@
-
-
-//createBundles(dataArray[0], dataArray[1], bundle[2] )
-tripBundles.createBundles = (transportation, hotels, activities) => {
+exports.createBundles = (transportation, hotels, activities, userData) => {
   var bundles = []
   var budgetDistribution = {transportation: 0.4, lodging: 0.4, activities: 0.2}
 
-  if(transportation.tripOption && transportation.tripOption.length > 0) {
+  if(transportation) {
     var addTransportation = []
 
     if(!bundles.length)
@@ -22,18 +19,14 @@ tripBundles.createBundles = (transportation, hotels, activities) => {
     bundles = addTransportation
   }
 
-  if(hotels.length >= 0) {
+  if(hotels.length > 0) {
     var addHotel = []
-    console.log("hotels before hotels.forEach: ", hotels)
-
     if(!bundles.length)
         bundles = [[{transportation: 'Travel by car!'}]]
 
     bundles.forEach(bundle => {
         hotels.forEach(hotel => {
-            if(Number(hotel.lowRate)  < budgetDistribution.lodging * req.body.budget) {
               var bundleCopy = bundle.slice()
-
               bundleCopy.push({ 'hotel':
                 {name: hotel.name,
                  address: hotel.address,
@@ -45,43 +38,73 @@ tripBundles.createBundles = (transportation, hotels, activities) => {
                 }
               })
               addHotel.push(bundleCopy)
-            }
         })
       })
 
     bundles = addHotel
   }
 
-  if(activities.length) {
+  if(activities && activities.length) {
     var addActivity = []
 
     if(!bundles.length)
       bundles = [[{'lodging': 'Stay with a Friend!'}, {'transportation': 'Travel by Car!'}]]
 
+    var activity1 = userData.answers[2].option.option
+    var activity2 = userData.answers[3].option.option
+
     bundles.forEach(bundle => {
       activities.forEach(activity => {
-        if(activity.category === 'Arcade' || activity.category === 'Campground') {
+        activity.forEach(act => {
 
-          var bundleCopy = bundle.slice()
-
-          bundleCopy.push({'activity': {
-              category: activity.category,
-              results: activity.results
-            }
-          })
-          addActivity.push(bundleCopy)
-        }
+          if(act.category === activity1) {
+            var bundleCopy = bundle.slice()
+            bundleCopy.push({'activity1': {
+                category: act.category,
+                results: act.results
+              }
+            })
+            addActivity.push(bundleCopy)
+          }
+        })
       })
     })
-    bundles = addActivity
+
+    if(addActivity.length)
+        bundles = addActivity
+    else
+      bundles.forEach( bundle => bundle.push({'activity': 'None found'}))
+
+    addActivity = []
+    bundles.forEach(bundle => {
+      activities.forEach(activity => {
+        activity.forEach(act => {
+
+          if(act.category === activity2) {
+
+            var bundleCopy = bundle.slice()
+            bundleCopy.push({'activity2': {
+                category: act.category,
+                results: act.results
+              }
+            })
+            addActivity.push(bundleCopy)
+          }
+        })
+
+      })
+    })
+
+    if(addActivity.length)
+        bundles = addActivity
+    else
+      bundles.forEach( bundle => bundle.push({'activity': 'None found'}))
   }
-  else
+
+  if(!bundles.length)
     bundles = [[{'transportation': 'Sorry nothing found'}, {'lodging': 'Sorry nothing found'}, {'activities': 'Sorry nothing found'}]]
 
   if(bundles.length >= 6)
     return bundles.slice(0,6)
   else return bundles
 }
-
-
-
